b0d8cfd044a146cf03f710ea788fcc4a880dce75
taisei.ito <taisei.ito@leverages.jp>
2024-04-15 18:14:44 +0900

    add

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+const IndexPage: NextPage = () => {
+    //useStateを使用し、状態を定義する
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+    //マウント時に画像を読み込む
+    useEffect(() => {
+        fetchImage().then((newImage) => {
+            setImageUrl(newImage.url);
+            setLoading(false);
+        });
+    }, []);
+    //ローディング中でなければ、画像を表示する
+    return <div>{loading || <img src={imageUrl} />}</div>;
+};
+export default IndexPage;
+
+const fetchImage = async () => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images [0];
+};
+
+fetchImage();
